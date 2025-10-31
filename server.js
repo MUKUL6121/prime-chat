88bffb64bfb848c145565aa0d89c86fc259e06a6
Mukul Jangra <148455498+MUKUL6121@users.noreply.github.com>
2025-10-31 19:06:24 +0530

    Add home route to start chat
    Add a route for the home page that links to the chat.

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/',(req,res)=>{
+    res.send("<a href="chat">Start Chat</a>
+});
 app.get('/chat', (req, res) => {
     console.log("Server is LIVE ......");
     res.sendFile(path.join(__dirname, 'public', 'chat.html'));
