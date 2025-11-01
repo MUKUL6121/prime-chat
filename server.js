25396831cf4f96033ad290f635bbe64787d556dd
Mukul Jangra <148455498+MUKUL6121@users.noreply.github.com>
2025-11-01 20:37:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,19 +10,25 @@ const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/',(req,res)=>{
-    res.send(`<a href="chat">Start Chat</a>`);
-});
+app.get('/', (req, res) => {
+    res.send(`<a href="chat">Start Chat</a>`)
+})
 app.get('/chat', (req, res) => {
     console.log("Server is LIVE ......");
     res.sendFile(path.join(__dirname, 'public', 'chat.html'));
     // res.end();
 });
 
+var users = 0;
+
 io.on('connection', (socket) => {
+    users++;
     console.log(`user Connected:`, socket.id);
+    io.emit('usercount', users);
     // console.log(socket);
     socket.on('disconnect', () => {
+        users--;
+        io.emit('usercount', users);
         console.log("A user Disconnected:", socket.id);
     });
     // socket.on('setuser_name', (client) => {
@@ -30,14 +36,15 @@ io.on('connection', (socket) => {
     //     console.log("User Name Set To: " + user_name);
     // });
     socket.on('chat message', (data) => {
-        // console.log(data.user + " : " + data.msg + " : " + data.time);
+        console.log(data.user + " : " + data.msg + " : " + data.time);
         // console.log(data);
         const msgary = [];
         msgary.push(data);
         io.emit('message', msgary);
     });
+
 });
 
 server.listen(2000, () => {
-    console.log(`http://localhost:2000/chat`);
+    console.log(`http://localhost:2000/`);
 });
