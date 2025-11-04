e7812af47a8da0e922f90bdc83830aef4329e73a
Mukul Jangra <148455498+MUKUL6121@users.noreply.github.com>
2025-11-04 12:19:38 +0530

    Enhance chat link and user connection logic
    Updated the chat start link to use an h2 tag and improved user connection handling.

--- server.js
+++ server.js
@@ -2,22 +2,21 @@ const express = require('express');
 const path = require('path');
 const { Server } = require('socket.io');
 const http = require('http');
+
 const PORT = process.env.PORT || 7800;
 
 const app = express();
-
 const server = http.createServer(app);
 const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.send(`<a href="chat">Start Chat</a>`)
+    res.send(`<a href="chat"><h2>Start Chat</h2></a>`)
 })
 app.get('/chat', (req, res) => {
     console.log("Server is LIVE ......");
     res.sendFile(path.join(__dirname, 'public', 'chat.html'));
-    // res.end();
 });
 
 var users = 0;
@@ -26,24 +25,21 @@ io.on('connection', (socket) => {
     users++;
     console.log(`user Connected:`, socket.id);
     io.emit('usercount', users);
-    // console.log(socket);
+    
     socket.on('disconnect', () => {
         users--;
         io.emit('usercount', users);
         console.log("A user Disconnected:", socket.id);
     });
-    // socket.on('setuser_name', (client) => {
-    //     const user_name = client.name;
-    //     console.log("User Name Set To: " + user_name);
-    // });
+   
     socket.on('chat message', (data) => {
         console.log(data.user + " : " + data.msg + " : " + data.time);
-        // console.log(data);
         const msgary = [];
         msgary.push(data);
         io.emit('message', msgary);
     });
 });
+
 app.listen(PORT,()=>{
     console.log("server is started");
 });
