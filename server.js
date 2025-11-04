b6572747fd6c2b5386a96e4b18566b2ce598c95d
Mukul Jangra <148455498+MUKUL6121@users.noreply.github.com>
2025-11-04 12:36:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,16 +3,17 @@ const path = require('path');
 const { Server } = require('socket.io');
 const http = require('http');
 
-const PORT = process.env.PORT || 7800;
+const PORT = process.env.PORT || 9890;
 
 const app = express();
+
 const server = http.createServer(app);
 const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.send(`<a href="chat"><h2>Start Chat</h2></a>`)
+    res.send(`<a href="chat">Start Chat</a>`)
 })
 app.get('/chat', (req, res) => {
     console.log("Server is LIVE ......");
@@ -25,21 +26,17 @@ io.on('connection', (socket) => {
     users++;
     console.log(`user Connected:`, socket.id);
     io.emit('usercount', users);
-    
-    socket.on('disconnect', () => {
-        users--;
-        io.emit('usercount', users);
-        console.log("A user Disconnected:", socket.id);
-    });
-   
+
     socket.on('chat message', (data) => {
         console.log(data.user + " : " + data.msg + " : " + data.time);
+        // console.log(data);
         const msgary = [];
         msgary.push(data);
         io.emit('message', msgary);
     });
+
 });
 
-app.listen(PORT,()=>{
-    console.log("server is started");
+server.listen(PORT, () => {
+    console.log(`Server is started`);
 });
